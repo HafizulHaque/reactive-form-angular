2508602ce44c2941bfa328355c9627b742cb6e3c
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2022-03-28 01:38:51 +0600

    form layout gets complicated with creation of dynamic control

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, Form, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -20,13 +20,30 @@ export class RegisterFormComponent implements OnInit {
         'userName': new FormControl(null, Validators.required),
         'userEmail': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'userGender': new FormControl('male')
+      'userGender': new FormControl('male'),
+      'userBirthday': new FormControl(null, [Validators.required]),
+      'userGradYear': new FormControl(null, [Validators.required, this.validateGradAfterBirthday]),
+      'hobbies': new FormArray([])
     });
   }
+
+  onAddHobby(){
+    let control : FormControl = new FormControl(null, Validators.required);
+    (<FormArray>this.registerForm.get('hobbies')).push(control);
+  }
+
 
   onSubmit(){
     console.log(this.registerForm);
     // this.registerForm.reset();
+  }
+
+  getHobbiesControl() : any {
+    return (<FormArray>this.registerForm.get('hobbies')).controls;
+  }
+
+  validateGradAfterBirthday(gradControl: FormControl): {[key: string]: any} | null {
+    return null;
   }
 
 }
